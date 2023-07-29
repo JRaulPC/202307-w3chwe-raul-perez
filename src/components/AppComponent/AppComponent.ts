849b934a55618fef7436fa7118674d044b1a1838
Raúl Pérez <jrpcassone@gmail.com>
2023-07-30 00:43:56 +0200

    Add parseDataFromApi method and add CardTest

--- src/components/AppComponent/AppComponent.ts
+++ src/components/AppComponent/AppComponent.ts
@@ -1,9 +1,24 @@
+import urlPokeApi from "../../globals.js";
+import {
+  type Pokemon,
+  type PokemonDataResponse,
+  type PokemonListResponse,
+} from "../../types.js";
 import Component from "../Component/Component.js";
 import PokemonCardComponent from "../PokemonCardComponent/PokemonCardComponent.js";
-import pokemonCardsCallList from "../data/pokemons.js";
+
 class AppComponent extends Component {
+  private pagePokemonsCall: Pokemon[] = [];
+
   constructor(parentElement: Element) {
     super(parentElement, "div", "container");
+
+    (async () => {
+      const pokemons: Pokemon[] = await this.parsePokemonData();
+
+      this.pagePokemonsCall = pokemons;
+      this.renderPokemonCards();
+    })();
   }
 
   public render(): void {
@@ -16,14 +31,40 @@ class AppComponent extends Component {
       <main class="main-page">
         <ul class="pokemons-list"></ul>
       </main>`;
+  }
 
+  private renderPokemonCards() {
     const pokemonList = this.element.querySelector(".pokemons-list")!;
-
-    pokemonCardsCallList.forEach((pokemonCard) => {
+    this.pagePokemonsCall.forEach((pokemonCard) => {
       const pokemonCardListElement = document.createElement("li");
       new PokemonCardComponent(pokemonCardListElement, pokemonCard).render();
       pokemonList.append(pokemonCardListElement);
     });
+  }
+
+  private async parsePokemonData() {
+    const pokemonListData = await fetch(urlPokeApi);
+
+    const { results: receivedListOfPokemons } =
+      (await pokemonListData.json()) as {
+        results: PokemonListResponse[];
+      };
+
+    const pagePokemons: Pokemon[] = [];
+
+    for await (const pokemon of receivedListOfPokemons) {
+      const individualPokemonResponse = await fetch(pokemon.url);
+      const pokemonData =
+        (await individualPokemonResponse.json()) as PokemonDataResponse;
+
+      pagePokemons.push({
+        id: pokemonData.id,
+        name: pokemonData.name,
+        pictureUrl: pokemonData.sprites.other.home.front_default,
+      });
+    }
+
+    return pagePokemons;
   }
 }
 
---
+++ src/components/PokemonCardComponent/PokemonCardComponent.test.ts
@@ -0,0 +1,22 @@
+import { type Pokemon } from "../../types";
+import PokemonCardComponent from "./PokemonCardComponent";
+
+describe("Given an PokemonCardComponent component", () => {
+  describe("when it receives a pokemon called 'bulbasaur' ", () => {
+    test("Then it should show 'bulbasaur' inside a heading ", () => {
+      const container = document.createElement("div");
+      const bulbasaur: Pokemon = {
+        id: 1,
+        name: "bulbasaur",
+        pictureUrl: "",
+      };
+
+      const cardComponent = new PokemonCardComponent(container, bulbasaur);
+      cardComponent.render();
+
+      const headingElement = container.querySelector("h2")!;
+
+      expect(headingElement.textContent).toBe(bulbasaur.name);
+    });
+  });
+});
