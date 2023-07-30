30dd8eb0738f3905e2a03903301954fb6abacf85
Raúl Pérez <jrpcassone@gmail.com>
2023-07-30 13:21:59 +0200

    Create PokemonPageComponent

---
+++ src/components/PokemonPage/PokemonPage.ts
@@ -0,0 +1,32 @@
+import { type Pokemon } from "../../types";
+import Component from "../Component/Component.js";
+import PokemonCardComponent from "../PokemonCardComponent/PokemonCardComponent.js";
+
+class PokemonPage extends Component {
+  constructor(
+    parentElement: Element,
+    private readonly pagePokemons: Pokemon[]
+  ) {
+    super(parentElement, "main", "main-page");
+  }
+
+  render() {
+    super.render();
+
+    this.element.innerHTML = `
+      <ul class="pokemons-list"></ul>
+    `;
+    this.renderPokemonCards();
+  }
+
+  private renderPokemonCards() {
+    const pokemonList = this.element.querySelector(".pokemons-list")!;
+    this.pagePokemons.forEach((pokemonCard) => {
+      const pokemonCardListElement = document.createElement("li");
+      new PokemonCardComponent(pokemonCardListElement, pokemonCard).render();
+      pokemonList.append(pokemonCardListElement);
+    });
+  }
+}
+
+export default PokemonPage;
