ce8a1e445467cfb6b13a837a1bab630bcbaf4e61
Raúl Pérez <jrpcassone@gmail.com>
2023-07-30 21:52:06 +0200

    Correct Uppercase pokemon letter

--- src/components/PokemonCardComponent/PokemonCardComponent.ts
+++ src/components/PokemonCardComponent/PokemonCardComponent.ts
@@ -3,16 +3,20 @@ import Component from "../Component/Component.js";
 
 class PokemonCardComponent extends Component {
   constructor(parentElement: Element, private readonly pokemon: Pokemon) {
-    super(parentElement, "article");
+    super(parentElement, "article", "pokemon-card");
   }
 
   render() {
     super.render();
 
     this.element.innerHTML = `
-        <h2>${this.pokemon.name}</h2>
-          <img src="${this.pokemon.pictureUrl}" alt="">
-        <span>${this.pokemon.id}</span>
+        <h2 class="pokemon-card__name">${
+          this.pokemon.name.charAt(0).toUpperCase() + this.pokemon.name.slice(1)
+        }</h2>
+          <img src="${this.pokemon.pictureUrl}" alt= an image of the pokemon"${
+      this.pokemon.name
+    }">
+        <span class="pokemon-card__id">${this.pokemon.id}</span>
     `;
   }
 }
